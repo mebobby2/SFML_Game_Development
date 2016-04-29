aaf5efc127f7b8764ddcf2c82c0516a2eb8b51d9
mebobby2 <bobl53@gmail.com>
2016-04-29 17:11:30 +1200

    moving the circle

--- SpaceWars/SpaceWars/Game.cpp
+++ SpaceWars/SpaceWars/Game.cpp
@@ -11,6 +11,10 @@
 Game::Game()
 : mWindow(sf::VideoMode(640, 480), "SpaceWars!")
 , mPlayer()
+, mIsMovingUp(false)
+, mIsMovingDown(false)
+, mIsMovingLeft(false)
+, mIsMovingRight(false)
 {
     mPlayer.setRadius(40.f);
     mPlayer.setPosition(100.f, 100.f);
@@ -32,13 +36,34 @@ void Game::processEvents()
     sf::Event event;
     while (mWindow.pollEvent(event))
     {
-        if (event.type == sf::Event::Closed)
-            mWindow.close();
+        switch (event.type) {
+            case sf::Event::KeyPressed:
+                handlePlayerInput(event.key.code, true);
+                break;
+            case sf::Event::KeyReleased:
+                handlePlayerInput(event.key.code, false);
+                break;
+            case sf::Event::Closed:
+                mWindow.close();
+                break;
+        }
     }
 }
 
 void Game::update()
 {
+    sf::Vector2f movement(0.f, 0.f);
+    if (mIsMovingUp)
+        movement.y -= 1.f;
+    if (mIsMovingDown)
+        movement.y += 1.f;
+    if (mIsMovingLeft)
+        movement.x -= 1.f;
+    if (mIsMovingRight)
+        movement.x += 1.f;
+    
+    mPlayer.move(movement);
+    
 }
 
 void Game::render()
@@ -46,4 +71,16 @@ void Game::render()
     mWindow.clear();
     mWindow.draw(mPlayer);
     mWindow.display();
+}
+
+void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed)
+{
+    if (key == sf::Keyboard::W)
+        mIsMovingUp = isPressed;
+    else if (key == sf::Keyboard::S)
+        mIsMovingDown = isPressed;
+    else if (key == sf::Keyboard::A)
+        mIsMovingLeft = isPressed;
+    else if (key == sf::Keyboard::D)
+        mIsMovingRight = isPressed;
 }
--- SpaceWars/SpaceWars/Game.hpp
+++ SpaceWars/SpaceWars/Game.hpp
@@ -22,9 +22,16 @@ class Game
         void    update();
         void    render();
     
+        void    handlePlayerInput(sf::Keyboard::Key key, bool isPressed);
+    
     private:
         sf::RenderWindow mWindow;
         sf::CircleShape mPlayer;
+    
+        bool mIsMovingUp;
+        bool mIsMovingDown;
+        bool mIsMovingRight;
+        bool mIsMovingLeft;
 };
 
 #endif /* defined(__SpaceWars__Game__) */
