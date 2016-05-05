f68028625f2766a44d2c92b372a8428e6cd0eb12
mebobby2 <bobl53@gmail.com>
2016-05-05 15:01:19 +1200

    implementing SceneNode’s draw method

--- SpaceWars/SpaceWars/SceneNode.cpp
+++ SpaceWars/SpaceWars/SceneNode.cpp
@@ -50,3 +50,18 @@ SceneNode::Ptr SceneNode::detachChild(const SceneNode &node)
     mChildren.erase(found);
     return result;
 }
+
+void SceneNode::draw(sf::RenderTarget &target, sf::RenderStates states) const
+{
+    states.transform *= getTransform();
+    drawCurrent(target, states);
+    
+    for (const Ptr& child : mChildren) {
+        child->draw(target, states);
+    }
+}
+
+void SceneNode::drawCurrent(sf::RenderTarget &target, sf::RenderStates states) const
+{
+    // Do nothing by default
+}
--- SpaceWars/SpaceWars/SceneNode.hpp
+++ SpaceWars/SpaceWars/SceneNode.hpp
@@ -9,10 +9,15 @@
 #ifndef __SpaceWars__SceneNode__
 #define __SpaceWars__SceneNode__
 
+#include <SFML/System/NonCopyable.hpp>
+#include <SFML/System/Time.hpp>
+#include <SFML/Graphics/Transformable.hpp>
+#include <SFML/Graphics/Drawable.hpp>
+
 #include <vector>
 #include <memory>
 
-class SceneNode
+class SceneNode : public sf::Transformable, public sf::Drawable, private sf::NonCopyable
 {
     public:
         typedef std::unique_ptr<SceneNode> Ptr;
@@ -21,6 +26,10 @@ class SceneNode
         SceneNode();
         void attachChild(Ptr child);
         Ptr detachChild(const SceneNode& node);
+    
+    private:
+        virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+        virtual void drawCurrent(sf::RenderTarget& target, sf::RenderStates states) const;
     
     private:
         std::vector<Ptr> mChildren;
