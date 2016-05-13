45f042ed516947e4c68464b37082ac3e7f910aab
Bobby Lei <bobl53@gmail.com>
2016-05-13 15:03:24 +1200

    added state identifiers

---
+++ SpaceWars/SpaceWars/StateIdentifiers.hpp
@@ -0,0 +1,25 @@
+//
+//  StateIdentifiers.hpp
+//  SpaceWars
+//
+//  Created by Erica Ko on 13/05/16.
+//  Copyright © 2016 mebobby. All rights reserved.
+//
+
+#ifndef StateIdentifiers_hpp
+#define StateIdentifiers_hpp
+
+namespace States
+{
+    enum ID
+    {
+        None,
+        Title,
+        Menu,
+        Game,
+        Loading,
+        Pause
+    };
+}
+
+#endif /* StateIdentifiers_hpp */
