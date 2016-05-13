5a8fd4dc4fe92dbc443afcb742e63e287aedca5c
Bobby Lei <bobl53@gmail.com>
2016-05-13 15:02:14 +1200

    added fonts into resource holder

--- SpaceWars/SpaceWars/ResourceIdentifiers.hpp
+++ SpaceWars/SpaceWars/ResourceIdentifiers.hpp
@@ -12,6 +12,7 @@
 namespace sf
 {
     class Texture;
+    class Font;
 }
 
 namespace Textures
@@ -21,12 +22,23 @@ namespace Textures
         Eagle,
         Rapter,
         Desert,
+        TitleScreen,
+    };
+}
+
+namespace Fonts
+{
+    enum ID
+    {
+        Main,
     };
 }
 
 template <typename Resource, typename Identifier>
 class ResourceHolder;
 
-typedef ResourceHolder<sf::Texture, Textures::ID> TextureHolder;;
+typedef ResourceHolder<sf::Texture, Textures::ID> TextureHolder;
+typedef ResourceHolder<sf::Font, Fonts::ID> FontHolder;
 
 #endif /* defined(__SpaceWars__ResourceIdentifiers__) */
+
