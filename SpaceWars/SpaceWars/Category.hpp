bc4b327c9e7003537e8aa194395fbf0afe7bfbb6
Bobby Lei <bobl53@gmail.com>
2016-05-21 11:19:40 +1200

    adding more categories

--- SpaceWars/SpaceWars/Category.hpp
+++ SpaceWars/SpaceWars/Category.hpp
@@ -17,14 +17,21 @@ namespace Category
     // And 1 << 2 = 0000 0100
     enum Type
     {
-        None           = 0,
-        Scene =          1 << 0,
-        PlayerAircraft = 1 << 1,
-        AlliedAircraft = 1 << 2,
-        EnemyAircraft  = 1 << 3,
+        None             = 0,
+        SceneAirLayer    = 1 << 0,
+        PlayerAircraft   = 1 << 1,
+        AlliedAircraft   = 1 << 2,
+        EnemyAircraft    = 1 << 3,
+        Pickup           = 1 << 4,
+        AlliedProjectile = 1 << 5,
+        EnemyProjectile  = 1 << 6,
+        
+        Aircraft         = PlayerAircraft | AlliedAircraft | EnemyAircraft,
+        Projectile       = AlliedProjectile | EnemyProjectile,
     };
 }
 
 #endif /* Category_hpp */
+
 
 
