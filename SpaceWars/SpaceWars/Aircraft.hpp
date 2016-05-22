d49151a68571023b9cc542bab0f351c0da097453
Bobby Lei <bobl53@gmail.com>
2016-05-22 20:44:56 +1200

    implementing Aircraft.hpp

--- SpaceWars/SpaceWars/Aircraft.hpp
+++ SpaceWars/SpaceWars/Aircraft.hpp
@@ -10,33 +10,76 @@
 #define __SpaceWars__Aircraft__
 
 #include "Entity.hpp"
-#include <SFML/Graphics/Sprite.hpp>
+#include "Command.hpp"
 #include "ResourceIdentifiers.hpp"
+#include "Projectile.hpp"
+#include "TextNode.hpp"
+
+#include <SFML/Graphics/Sprite.hpp>
 
 class Aircraft: public Entity
 {
-    public:
-        enum Type
-        {
-            Eagle,
-            Rapter,
-            Avenger,
-            TypeCount
-        };
+public:
+    enum Type
+    {
+        Eagle,
+        Rapter,
+        Avenger,
+        TypeCount
+    };
     
-    public:
-            Aircraft(Type type, const TextureHolder& textures);
-        virtual void drawCurrent(sf::RenderTarget& target, sf::RenderStates states) const;
-        virtual unsigned int getCategory() const;
+public:
+    Aircraft(Type type, const TextureHolder& textures, const FontHolder& fonts);
     
-        void increaseFireRate();
-        void increaseSpread();
-        void collectMissiles(unsigned int count);
+    virtual void drawCurrent(sf::RenderTarget& target, sf::RenderStates states) const;
+    virtual void updateCurrent(sf::Time dt, CommandQueue& commands);
+    virtual unsigned int getCategory() const;
+    virtual sf::FloatRect getBoundingRect() const;
+    virtual bool isMarkedForRemoval() const;
+    bool isAllied() const;
+    float getMaxSpeed() const;
     
-    private:
-        Type mType;
-        sf::Sprite mSprite;
+    void increaseFireRate();
+    void increaseSpread();
+    void collectMissiles(unsigned int count);
+    
+    void fire();
+    void launchMissile();
+    
+private:
+    void updateMovementPattern(sf::Time dt);
+    void checkPickupDrop(CommandQueue& commands);
+    void checkProjectileLaunch(sf::Time dt, CommandQueue& commands);
+    
+    void createBullets(SceneNode& node, const TextureHolder& textures) const;
+    void createProjectile(SceneNode& node, Projectile::Type type, float xOffset, float yOffset, const TextureHolder& textures) const;
+    void createPickup(SceneNode& node, const TextureHolder& textures) const;
+    
+    void updateTexts();
+    
+    
+private:
+    Type mType;
+    sf::Sprite mSprite;
+    Command mFireCommand;
+    Command mMissileCommand;
+    sf::Time mFireCountdown;
+    bool mIsFiring;
+    bool mIsLaunchingMissile;
+    bool mIsMarkedForRemoval;
+    
+    int mFireRateLevel;
+    int mSpreadLevel;
+    int mMissibleAmmo;
+    
+    Command mDropPickupCommand;
+    float mTravelledDistance;
+    std::size_t mDirectionIndex;
+    TextNode* mHealthDisplay;
+    TextNode* mMissileDisplay;
+    
 };
 
 
 #endif /* defined(__SpaceWars__Aircraft__) */
+
