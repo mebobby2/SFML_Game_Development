5fe514e96563a8b68a60d3796430c87830ad397c
Bobby Lei <bobl53@gmail.com>
2016-06-30 22:55:37 +0800

    added resource identifiers for sound and music

--- SpaceWars/SpaceWars/ResourceIdentifiers.hpp
+++ SpaceWars/SpaceWars/ResourceIdentifiers.hpp
@@ -14,6 +14,7 @@ namespace sf
     class Texture;
     class Font;
     class Shader;
+    class SoundBuffer;
 }
 
 namespace Textures
@@ -49,12 +50,35 @@ namespace Fonts
     };
 }
 
+namespace SoundEffect
+{
+    enum ID
+    {
+        AlliedGunfire,
+        EnemyGunfire,
+        Explosion1,
+        Explosion2,
+        LaunchMissile,
+        CollectPickup,
+        Button,
+    };
+}
+
+namespace Music
+{
+    enum ID
+    {
+        MenuTheme,
+        MissionTheme,
+    };
+}
+
 template <typename Resource, typename Identifier>
 class ResourceHolder;
 
 typedef ResourceHolder<sf::Texture, Textures::ID> TextureHolder;
 typedef ResourceHolder<sf::Font, Fonts::ID> FontHolder;
 typedef ResourceHolder<sf::Shader, Shaders::ID> ShaderHolder;
+typedef ResourceHolder<sf::SoundBuffer, SoundEffect::ID> SoundBufferHolder;
 
 #endif /* defined(__SpaceWars__ResourceIdentifiers__) */
-
